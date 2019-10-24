0f24467e52122cef8a39ebbe6da38f5317bd19c2
Jeffrey Mann <jwmann22@gmail.com>
2019-10-24 18:16:40 -0400

    initial file setup and command structure applied

---
+++ assets/js/bot.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const keys = require("./keys.js");
+
+const spotify = new Spotify(keys.spotify);
+
+function commands() {
+    switch (command) {
+        case 'concert-this':
+
+            break;
+
+        case 'spotify-this-song':
+
+            break;
+
+        case 'movie-this':
+
+            break;
+
+        case 'do-what-it-says':
+
+            break;
+
+        default:
+            break;
+    }
+}
